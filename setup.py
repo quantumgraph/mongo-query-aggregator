3957515eca878b6d92afdf65a68f382289faee9e
ravi_teja_n <ravi@quantumgraph.com>
2016-10-21 15:08:36 +0530

    updating version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     # This is the name of your PyPI-package.
     name='moquag',
     # Update the version number for new releases
-    version='0.23',
+    version='0.241',
     # The name of your scipt, and also the command you'll be using for
     # calling it
     # scripts=['moquag'],
